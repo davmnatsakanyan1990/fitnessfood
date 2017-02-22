a369187bc4befc34ae13a0ec10a8f8b365e0e843
davmnatsakanyan1990 <dav.mnatsakanyan@gmail.com>
2017-02-23 00:11:25 +0400

    pages table seeder

--- database/seeds/PagesTableSeeder.php
+++ database/seeds/PagesTableSeeder.php
@@ -13,30 +13,7 @@ class PagesTableSeeder extends Seeder
     public function run()
     {
         DB::table('pages')->insert([
-            ['title' => 'about us', 'content' => '<div class="responsive-height-block"><!-- Important --></div>
-    <main class="about-main">
-    <div class="container">
-        <div class="row text-center">
-            <div>
-                <img src="/images/about/about-logo.png" alt="about-logo.png">
-            </div>
-            <div class="col-sm-6">
-                <h1>Մեր Մասին</h1>
-                <p>
-                    Fitness Cook Ապրանքանիշը ներկայացնում է թխվածքների և հացերի տեսականի, որոնք Չեն Պարունակում ալյուր, շաքար (Sugar Free), գլյուտեն (Gluten Free), թթխմորներ (Дрожжи), ունեն շատ ցածր կալորիականություն և պատրաստվում են 100% բնական հումքից։
-                    <br><br>
-                    Ալյուրի փոխարեն մենք օգտագործում ենք վարսակի թեփ (Овсяные Отруби), իսկ շաքարի փոխարեն օգտագործում ենք Ստեվիա (стевия), ամեն ինչ պատրաստվում է անյուղ կաթնամթերքից և չի պարունակում կարագ և քիմիական հավելանյութեր։
-                    <br><br>
-                    Եղեք Առողջ և Քաղցրակեր :)
-                </p>
-            </div>
-            <div class="col-sm-6">
-                <div class="video-wrap">
-                    <iframe width="100%" height="280" src="https://www.youtube.com/embed/5Zzz2ZyjigU" frameborder="0" allowfullscreen></iframe>
-                </div>
-            </div>
-        </div>
-    </div>']
+            ['title' => 'about us', 'content' => '{"am":"\n                        <div class=\"responsive-height-block\"><!-- Important --><\/div>\n    <main class=\"about-main\">\n    <div class=\"container\">\n        <div class=\"row text-center\">\n            <div>\n                <img src=\"\/images\/about\/about-logo.png\" alt=\"about-logo.png\">\n            <\/div>\n            <div class=\"col-sm-6\">\n                <h1>Մեր Մասին<\/h1>\n                <p>\n                    Fitness Cook Ապրանքանիշը ներկայացնում է թխվածքների և հացերի տեսականի, որոնք Չեն Պարունակում ալյուր, շաքար (Sugar Free), գլյուտեն (Gluten Free), թթխմորներ (Дрожжи), ունեն շատ ցածր կալորիականություն և պատրաստվում են 100% բնական հումքից։\n                    <br><br>\n                    Ալյուրի փոխարեն մենք օգտագործում ենք վարսակի թեփ (Овсяные Отруби), իսկ շաքարի փոխարեն օգտագործում ենք Ստեվիա (стевия), ամեն ինչ պատրաստվում է անյուղ կաթնամթերքից և չի պարունակում կարագ և քիմիական հավելանյութեր։\n                    <br><br>\n                    Եղեք Առողջ և Քաղցրակեր :)\n                <\/p>\n            <\/div>\n            <div class=\"col-sm-6\">\n                <div class=\"video-wrap\">\n                    <iframe width=\"100%\" height=\"280\" src=\"https:\/\/www.youtube.com\/embed\/5Zzz2ZyjigU\" frameborder=\"0\" allowfullscreen=\"\"><\/iframe>\n                <\/div>\n            <\/div>\n        <\/div>\n    <\/div>\n                    <\/main>","ru":"\n                        <div class=\"responsive-height-block\"><!-- Important --><\/div>\n    <main class=\"about-main\">\n    <div class=\"container\">\n        <div class=\"row text-center\">\n            <div>\n                <img src=\"\/images\/about\/about-logo.png\" alt=\"about-logo.png\">\n            <\/div>\n            <div class=\"col-sm-6\">\n                <h1>Մեր Մասին<\/h1>\n                <p>\n                    Fitness Cook Ապրանքանիշը ներկայացնում է թխվածքների և հացերի տեսականի, որոնք Չեն Պարունակում ալյուր, շաքար (Sugar Free), գլյուտեն (Gluten Free), թթխմորներ (Дрожжи), ունեն շատ ցածր կալորիականություն և պատրաստվում են 100% բնական հումքից։\n                    <br><br>\n                    Ալյուրի փոխարեն մենք օգտագործում ենք վարսակի թեփ (Овсяные Отруби), իսկ շաքարի փոխարեն օգտագործում ենք Ստեվիա (стевия), ամեն ինչ պատրաստվում է անյուղ կաթնամթերքից և չի պարունակում կարագ և քիմիական հավելանյութեր։\n                    <br><br>\n                    Եղեք Առողջ և Քաղցրակեր :)\n                <\/p>\n            <\/div>\n            <div class=\"col-sm-6\">\n                <div class=\"video-wrap\">\n                    <iframe width=\"100%\" height=\"280\" src=\"https:\/\/www.youtube.com\/embed\/5Zzz2ZyjigU\" frameborder=\"0\" allowfullscreen=\"\"><\/iframe>\n                <\/div>\n            <\/div>\n        <\/div>\n    <\/div>\n                <\/main>","en":"\n                        <div class=\"responsive-height-block\"><!-- Important --><\/div>\n    <main class=\"about-main\">\n    <div class=\"container\">\n        <div class=\"row text-center\">\n            <div>\n                <img src=\"\/images\/about\/about-logo.png\" alt=\"about-logo.png\">\n            <\/div>\n            <div class=\"col-sm-6\">\n                <h1>Մեր Մասին<\/h1>\n                <p>\n                    Fitness Cook Ապրանքանիշը ներկայացնում է թխվածքների և հացերի տեսականի, որոնք Չեն Պարունակում ալյուր, շաքար (Sugar Free), գլյուտեն (Gluten Free), թթխմորներ (Дрожжи), ունեն շատ ցածր կալորիականություն և պատրաստվում են 100% բնական հումքից։\n                    <br><br>\n                    Ալյուրի փոխարեն մենք օգտագործում ենք վարսակի թեփ (Овсяные Отруби), իսկ շաքարի փոխարեն օգտագործում ենք Ստեվիա (стевия), ամեն ինչ պատրաստվում է անյուղ կաթնամթերքից և չի պարունակում կարագ և քիմիական հավելանյութեր։\n                    <br><br>\n                    Եղեք Առողջ և Քաղցրակեր :)\n                <\/p>\n            <\/div>\n            <div class=\"col-sm-6\">\n                <div class=\"video-wrap\">\n                    <iframe width=\"100%\" height=\"280\" src=\"https:\/\/www.youtube.com\/embed\/5Zzz2ZyjigU\" frameborder=\"0\" allowfullscreen=\"\"><\/iframe>\n                <\/div>\n            <\/div>\n        <\/div>\n    <\/div>\n                    <\/main>"}']
         ]);
     }
 }
