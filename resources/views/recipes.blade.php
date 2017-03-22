bef48b1b942ead7108f1d236e5bdfea165c454b4
aramgh <aram.17@mail.ru>
2017-03-23 01:13:12 +0400

    receptnery added

--- resources/views/recipes.blade.php
+++ resources/views/recipes.blade.php
@@ -3,7 +3,79 @@
 
 @endsection
 @section('content')
-    Here can be your new html :)
+	<div class="responsive-height-block"><!-- Important --></div>
+    <main>
+    	<div class="container">
+    		<div class="row">
+    			<div class="col-sm-3">
+    				<div class="recp-wrap">
+	    				<div class="for-reciep-img"><!-- For img bg --></div>
+	    				<h3>Award Winning Soft Chocolate Chip </h3>
+	    				<hr>
+	    				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam voluptatibus fugit, quos harum quibusdam ex.</p>
+    				</div>
+    			</div>
+    			<div class="col-sm-3">
+    				<div class="recp-wrap">
+	    				<div class="for-reciep-img"><!-- For img bg --></div>
+	    				<h3>Award Winning Soft Chocolate Chip </h3>
+	    				<hr>
+	    				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam voluptatibus fugit, quos harum quibusdam ex.</p>
+    				</div>
+    			</div>
+    			<div class="col-sm-3">
+    				<div class="recp-wrap">
+	    				<div class="for-reciep-img"><!-- For img bg --></div>
+	    				<h3>Award Winning Soft Chocolate Chip </h3>
+	    				<hr>
+	    				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam voluptatibus fugit, quos harum quibusdam ex.</p>
+    				</div>
+    			</div>
+    			<div class="col-sm-3">
+    				<div class="recp-wrap">
+	    				<div class="for-reciep-img"><!-- For img bg --></div>
+	    				<h3>Award Winning Soft Chocolate Chip </h3>
+	    				<hr>
+	    				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam voluptatibus fugit, quos harum quibusdam ex.</p>
+    				</div>
+    			</div>
+    		</div>
+    		    		<div class="row">
+    		    			<div class="col-sm-3">
+    		    				<div class="recp-wrap">
+    			    				<div class="for-reciep-img"><!-- For img bg --></div>
+    			    				<h3>Award Winning Soft Chocolate Chip </h3>
+    			    				<hr>
+    			    				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam voluptatibus fugit, quos harum quibusdam ex.</p>
+    		    				</div>
+    		    			</div>
+    		    			<div class="col-sm-3">
+    		    				<div class="recp-wrap">
+    			    				<div class="for-reciep-img"><!-- For img bg --></div>
+    			    				<h3>Award Winning Soft Chocolate Chip </h3>
+    			    				<hr>
+    			    				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam voluptatibus fugit, quos harum quibusdam ex.</p>
+    		    				</div>
+    		    			</div>
+    		    			<div class="col-sm-3">
+    		    				<div class="recp-wrap">
+    			    				<div class="for-reciep-img"><!-- For img bg --></div>
+    			    				<h3>Award Winning Soft Chocolate Chip </h3>
+    			    				<hr>
+    			    				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam voluptatibus fugit, quos harum quibusdam ex.</p>
+    		    				</div>
+    		    			</div>
+    		    			<div class="col-sm-3">
+    		    				<div class="recp-wrap">
+    			    				<div class="for-reciep-img"><!-- For img bg --></div>
+    			    				<h3>Award Winning Soft Chocolate Chip </h3>
+    			    				<hr>
+    			    				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam voluptatibus fugit, quos harum quibusdam ex.</p>
+    		    				</div>
+    		    			</div>
+    		    		</div>
+    	</div>
+    </main>
 @endsection
 @section('scripts')
 
